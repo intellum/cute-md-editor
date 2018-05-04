5b81be9a3064c8c822745d615bdb1d4c5ab520bb
Jon Cairns <jon@joncairns.com>
2018-05-04 23:02:12 +0100

    Tidy up some styles, add preview loading spinner

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -5,18 +5,25 @@ export default class MarkdownEditor extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      html: ""
+      html: "",
+      loaded: false
     }
   }
 
   componentDidMount() {
     this.props.converter(this.props.markdown).
-      then((html) => this.setState({html: html})).
+      then((html) => this.setState({html: html, loaded: true})).
       catch((err) => this.setState({html: `Error: ${err.toString()}`}))
   }
 
   render() {
-    if (this.props.asHTML) {
+    if (this.state.loaded === false) {
+      return (
+        <div className="react-md-preview-area">
+          <div className="react-md-loader">Loading preview...</div>
+        </div>
+      )
+    } else if (this.props.asHTML) {
       return (
         <div className="react-md-preview-area">
           {this.state.html}
